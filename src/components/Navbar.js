ec77bb0898d0cd2e5c8500c1e0dfcbd5a7dc8476
Adithya2310 <Adithyanandi099@gmail.com>
2023-05-09 16:15:10 +0530

    updated resume link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ const Navbar = ({darkMode,setdarkMode}) => {
     <ul className='flex align-middle'>
       <li><BsMoonStarsFill onClick={()=>setdarkMode(!darkMode)} className='mt-3 cursor-pointer font-burtons' size={23}/></li>
       <li className='lg:ml-10 ml-5 px-4 py-2 rounded-md bg-gradient-to-r from-cyan-500 to-teal-500 cursor-pointer'>
-      <a href="https://drive.google.com/drive/u/0/my-drive">Resume</a> </li>
+      <a href="https://drive.google.com/file/d/1xk8rlXcqK2wlUKPqjHR09NssWky1LZ5B/view?usp=sharing">Resume</a> </li>
     </ul>
   </nav></div>
   )
